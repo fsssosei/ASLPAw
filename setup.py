ee2c0dcd31664984b372908bef0eeb162cfe81e7
fsssosei <43291778+fsssosei@users.noreply.github.com>
2019-11-09 02:38:50 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+'''
+ASLPAw - This is a Adaptive Speaker-listener Lable Propagation Algorithm package.
+Copyright (C) 2019  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='ASLPAw',
+    version='1.0.0',
+    description='Adaptive overlapping community discovery algorithm package in python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fsssosei/ASLPAw',
+    license='GNU Affero General Public License v3',
+    author='sosei',
+    author_email='fss.sosei@gmail.com',
+    keywords=['community', 'LPA'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    install_requires=['networkx>=2.4',
+        'shuffle-graph>=1.0.4',
+        'multivalued-dict>=1.6.1',
+        'count-dict>=1.0.1']
+)
