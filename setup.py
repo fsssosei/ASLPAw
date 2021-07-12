1418ed895999dca3ef424d0da181006554fed597
fsssosei <43291778+fsssosei@users.noreply.github.com>
2021-07-12 08:41:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='ASLPAw',
-    version='2.1.1',
+    version='2.2.0',
     description='Adaptive overlapping community discovery algorithm package in python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -44,7 +44,7 @@ setup(
     install_requires=[
         'networkx>=2.4',
         'multivalued-dict>=2.0.1',
-        'shuffle-graph>=2.1.0',
+        'shuffle-graph>=2.2.0',
         'count-dict>=1.1.1',
         'similarity-index-of-label-graph>=2.0.1',
         'scikit-learn>=0.24.1'
